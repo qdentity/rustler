784a6faf8701482f66e57b7c32674254e13f63de
hengermax <max@qdentity.nl>
2024-08-06 14:12:19 +0200

    Implement renaming for enums

--- rustler_codegen/src/tagged_enum.rs
+++ rustler_codegen/src/tagged_enum.rs
@@ -1,11 +1,18 @@
 use proc_macro2::{Span, TokenStream};
 use quote::{quote, quote_spanned};
 
-use heck::ToSnakeCase;
 use std::collections::HashMap;
 use syn::{self, spanned::Spanned, Field, Fields, FieldsNamed, FieldsUnnamed, Ident, Variant};
 
 use super::context::Context;
+
+#[derive(Hash, Eq, PartialEq)]
+enum AtomKey<'a> {
+    Variant(&'a Ident),
+    Field(&'a Ident, &'a Ident),
+}
+
+type AtomLookup<'a> = HashMap<AtomKey<'a>, (String, Ident)>;
 
 pub fn transcoder_decorator(ast: &syn::DeriveInput) -> TokenStream {
     let ctx = Context::from_ast(ast);
@@ -15,57 +22,50 @@ pub fn transcoder_decorator(ast: &syn::DeriveInput) -> TokenStream {
         .as_ref()
         .expect("NifTaggedEnum can only be used with enums");
 
-    let atoms = variants
-        .iter()
-        .flat_map(|variant| {
-            let mut ret = if let Fields::Named(fields) = &variant.fields {
-                fields
-                    .named
-                    .iter()
-                    .map(|field| {
-                        field
-                            .ident
-                            .as_ref()
-                            .expect("Named fields must have an ident.")
-                    })
-                    .collect::<Vec<_>>()
-            } else {
-                vec![]
-            };
+    // Build a collection of all field/tag idents, and potentially the strings
+    // they're renamed to.
+    let mut atom_lookup = AtomLookup::new();
+    for variant in variants {
+        let name = Context::name_from_attrs_or_ident(&variant.ident, &variant.attrs);
+        let atom = Context::string_to_atom_fun(&name);
+        atom_lookup.insert(AtomKey::Variant(&variant.ident), (name, atom));
 
-            ret.push(&variant.ident);
-            ret
-        })
-        .map(|atom_ident| {
-            let atom_str = atom_ident.to_string().to_snake_case();
-            let atom_fn = Context::ident_to_atom_fun(atom_ident);
-            (atom_str, atom_fn)
-        })
-        .collect::<HashMap<_, _>>()
-        .into_iter()
-        .map(|(atom_str, atom_fn)| {
-            quote! {
-                #atom_fn = #atom_str,
+        if let Fields::Named(fields) = &variant.fields {
+            for field in &fields.named {
+                let field_ident = field.ident.as_ref().expect("Named fields must have an ident.");
+                let name = Context::name_from_attrs_or_ident(&field_ident, &field.attrs);
+                let atom = Context::string_to_atom_fun(&name);
+                atom_lookup.insert(AtomKey::Field(&variant.ident, field_ident), (name, atom));
             }
-        })
-        .collect::<Vec<_>>();
+        }
+    }
 
+    // Produce code that defines the atoms
+    let mut known = Vec::new();
+    let mut atom_assignments = Vec::new();
+    for (_, (name, atom)) in atom_lookup.iter() {
+        let name = name.as_str();
+        if !known.contains(&name) {
+            known.push(name);
+            atom_assignments.push(quote!{ #atom = #name, })
+        }
+    }
     let atom_defs = quote! {
         ::rustler::atoms! {
-            #(#atoms)*
+            #(#atom_assignments)*
         }
     };
 
     let atoms_module_name = ctx.atoms_module_name(Span::call_site());
 
     let decoder = if ctx.decode() {
-        gen_decoder(&ctx, variants, &atoms_module_name)
+        gen_decoder(&ctx, variants, &atom_lookup, &atoms_module_name)
     } else {
         quote! {}
     };
 
     let encoder = if ctx.encode() {
-        gen_encoder(&ctx, variants, &atoms_module_name)
+        gen_encoder(&ctx, variants, &atom_lookup, &atoms_module_name)
     } else {
         quote! {}
     };
@@ -83,13 +83,13 @@ pub fn transcoder_decorator(ast: &syn::DeriveInput) -> TokenStream {
     gen
 }
 
-fn gen_decoder(ctx: &Context, variants: &[&Variant], atoms_module_name: &Ident) -> TokenStream {
+fn gen_decoder(ctx: &Context, variants: &[&Variant], atom_lookup: &AtomLookup, atoms_module_name: &Ident) -> TokenStream {
     let enum_name = ctx.ident;
     let unit_decoders: Vec<TokenStream> = variants
         .iter()
         .filter_map(|variant| {
             let variant_ident = &variant.ident;
-            let atom_fn = Context::ident_to_atom_fun(variant_ident);
+            let (_, atom_fn) = atom_lookup.get(&AtomKey::Variant(variant_ident)).unwrap();
             match &variant.fields {
                 Fields::Unit => Some(gen_unit_decoder(enum_name, variant_ident, atom_fn)),
                 _ => None,
@@ -100,7 +100,7 @@ fn gen_decoder(ctx: &Context, variants: &[&Variant], atoms_module_name: &Ident) 
         .iter()
         .filter_map(|variant| {
             let variant_ident = &variant.ident;
-            let atom_fn = Context::ident_to_atom_fun(variant_ident);
+            let (_, atom_fn) = atom_lookup.get(&AtomKey::Variant(variant_ident)).unwrap();
             match &variant.fields {
                 Fields::Unnamed(fields) => Some(gen_unnamed_decoder(
                     enum_name,
@@ -110,7 +110,7 @@ fn gen_decoder(ctx: &Context, variants: &[&Variant], atoms_module_name: &Ident) 
                     atom_fn,
                 )),
                 Fields::Named(fields) => {
-                    Some(gen_named_decoder(enum_name, fields, variant_ident, atom_fn))
+                    Some(gen_named_decoder(enum_name, fields, variant_ident, atom_lookup, atom_fn))
                 }
                 _ => None,
             }
@@ -154,14 +154,14 @@ fn gen_decoder(ctx: &Context, variants: &[&Variant], atoms_module_name: &Ident) 
     )
 }
 
-fn gen_encoder(ctx: &Context, variants: &[&Variant], atoms_module_name: &Ident) -> TokenStream {
+fn gen_encoder(ctx: &Context, variants: &[&Variant], atom_lookup: &AtomLookup, atoms_module_name: &Ident) -> TokenStream {
     let enum_name = ctx.ident;
 
     let variant_defs: Vec<TokenStream> = variants
         .iter()
         .map(|variant| {
             let variant_ident = &variant.ident;
-            let atom_fn = Context::ident_to_atom_fun(variant_ident);
+            let (_, atom_fn) = atom_lookup.get(&AtomKey::Variant(&variant.ident)).unwrap();
 
             match &variant.fields {
                 Fields::Unit => gen_unit_encoder(enum_name, variant_ident, atom_fn),
@@ -169,7 +169,7 @@ fn gen_encoder(ctx: &Context, variants: &[&Variant], atoms_module_name: &Ident) 
                     gen_unnamed_encoder(enum_name, fields, variant_ident, atom_fn)
                 }
                 Fields::Named(fields) => {
-                    gen_named_encoder(enum_name, fields, variant_ident, atom_fn)
+                    gen_named_encoder(enum_name, fields, variant_ident, &atom_lookup, atom_fn)
                 }
             }
         })
@@ -187,7 +187,7 @@ fn gen_encoder(ctx: &Context, variants: &[&Variant], atoms_module_name: &Ident) 
     )
 }
 
-fn gen_unit_decoder(enum_name: &Ident, variant_ident: &Ident, atom_fn: Ident) -> TokenStream {
+fn gen_unit_decoder(enum_name: &Ident, variant_ident: &Ident, atom_fn: &Ident) -> TokenStream {
     quote! {
         if unit == #atom_fn() {
             return Ok ( #enum_name :: #variant_ident );
@@ -200,7 +200,7 @@ fn gen_unnamed_decoder<'a>(
     fields: &FieldsUnnamed,
     fields_iter: impl Iterator<Item = &'a Field>,
     variant_ident: &Ident,
-    atom_fn: Ident,
+    atom_fn: &Ident,
 ) -> TokenStream {
     let decoded_field = &fields_iter
         .enumerate()
@@ -232,7 +232,8 @@ fn gen_named_decoder(
     enum_name: &Ident,
     fields: &FieldsNamed,
     variant_ident: &Ident,
-    atom_fn: Ident,
+    atom_lookup: &AtomLookup,
+    atom_fn: &Ident,
 ) -> TokenStream {
     let (assignments, field_defs): (Vec<TokenStream>, Vec<TokenStream>) = fields
         .named
@@ -243,7 +244,7 @@ fn gen_named_decoder(
                 .ident
                 .as_ref()
                 .expect("Named fields must have an ident.");
-            let atom_fun = Context::field_to_atom_fun(field);
+            let (_, atom_fun) = atom_lookup.get(&AtomKey::Field(variant_ident, ident)).unwrap();
             let variable = Context::escape_ident_with_index(&ident.to_string(), index, "map");
 
             let ident_string = ident.to_string();
@@ -276,7 +277,7 @@ fn gen_named_decoder(
     }
 }
 
-fn gen_unit_encoder(enum_name: &Ident, variant_ident: &Ident, atom_fn: Ident) -> TokenStream {
+fn gen_unit_encoder(enum_name: &Ident, variant_ident: &Ident, atom_fn: &Ident) -> TokenStream {
     quote! {
         #enum_name :: #variant_ident => ::rustler::Encoder::encode(&#atom_fn(), env),
     }
@@ -286,7 +287,7 @@ fn gen_unnamed_encoder(
     enum_name: &Ident,
     fields: &FieldsUnnamed,
     variant_ident: &Ident,
-    atom_fn: Ident,
+    atom_fn: &Ident,
 ) -> TokenStream {
     let len = fields.unnamed.len();
     let inners = (0..len)
@@ -301,7 +302,8 @@ fn gen_named_encoder(
     enum_name: &Ident,
     fields: &FieldsNamed,
     variant_ident: &Ident,
-    atom_fn: Ident,
+    atom_lookup: &AtomLookup,
+    atom_fn: &Ident,
 ) -> TokenStream {
     let field_decls = fields
         .named
@@ -321,7 +323,7 @@ fn gen_named_encoder(
                 .ident
                 .as_ref()
                 .expect("Named fields must have an ident.");
-            let atom_fun = Context::field_to_atom_fun(field);
+            let (_, atom_fun) = atom_lookup.get(&AtomKey::Field(variant_ident, field_ident)).unwrap();
             (
                 quote! { ::rustler::Encoder::encode(&#atom_fun(), env) },
                 quote! { ::rustler::Encoder::encode(&#field_ident, env) },
